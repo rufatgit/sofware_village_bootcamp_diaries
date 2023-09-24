77f49852cfd2600d9a5cd4ff3fe6d5a8d8b673c4
rufatgit <rufat.valiyev02@gmail.com>
2023-09-24 11:15:03 +0200

    4home

---
+++ lesson4 homework/lesson4 homework/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+public class Program
+{
+    public static void Space(int a)
+    {
+        int[] dividents = new int[2];
+
+
+        dividents[0] = a / 10;
+        dividents[1] = a % 10;
+
+        Console.WriteLine("*****");
+        for (int i = 0; i < dividents.Length; i++)
+        {
+            Console.Write(dividents[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+    public static void Digits(int b)
+    {
+        int[] dividents = { 10,100,1000,10000,100000, 1000000, 10000000, 100000000};
+        int counter=1;
+
+        for (int i = 0; i < dividents.Length; i++)
+        {
+            if (b % dividents[i] != b)
+            {
+                ++counter;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Number of digits is " + counter);
+
+    }
+
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        Space(a);
+
+        int b=int.Parse(Console.ReadLine());
+        Digits(b);
+
+
+
+    }
+}
