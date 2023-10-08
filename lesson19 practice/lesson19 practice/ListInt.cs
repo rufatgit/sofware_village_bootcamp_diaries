1594e5d76bab98bcedb6592bb9cca74c503adab7
rufatgit <rufat.valiyev02@gmail.com>
2023-10-08 10:55:09 +0200

    19

---
+++ lesson19 practice/lesson19 practice/ListInt.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson19_practice
+{
+    public class ListInt
+    {
+        private int[] array = new int[0];
+        public int Length
+        {
+            get { return array.Length; }
+        }
+
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < array.Length)
+                {
+                    return array[index];
+                }
+                else
+                {
+                    return array[array.Length - 1];
+                }
+            }
+
+            set
+            {
+                if (array.Length > index)
+                {
+                    array[index] = value;
+                }
+            }
+        }
+
+
+
+        public void Add(int num)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = num;
+        }
+
+
+        public void AddRange(params int[] nums)
+        {
+            Array.Resize(ref array, array.Length + nums.Length);  //
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                array[array.Length - nums.Length + i] = nums[i];  //3-2+0    1 2 3 - 5 6
+            }
+
+
+
+        }
+
+
+
+    }
+}
---
+++ lesson19 practice/lesson19 practice/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace lesson19_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ListInt intt = new ListInt();
+            intt.Add(1);
+            intt.Add(2);
+            intt.Add(3);
+
+
+            intt.AddRange(new int[2] { 5, 6 });
+
+            for (int i = 0; i <intt.Length; i++)
+            {
+                Console.WriteLine(intt[i]);
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
